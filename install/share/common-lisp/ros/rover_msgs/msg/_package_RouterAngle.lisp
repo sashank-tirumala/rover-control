18d3b32cc28e834d8d8006fca68edb4fe703dd7f
Sashank Tirumala <stsashank6@gmail.com>
2018-02-03 20:27:54 +0530

    Fixed a bug that prevented catkin_make

--- install/share/common-lisp/ros/rover_msgs/msg/_package_RouterAngle.lisp
+++
@@ -1,4 +0,0 @@
-(cl:in-package rover_msgs-msg)
-(cl:export '(ANGLE-VAL
-          ANGLE
-))
--- install/share/common-lisp/ros/rover_msgs/msg/_package_WheelVelocity.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package rover_msgs-msg)
-(cl:export '(LEFT-VAL
-          LEFT
-          RIGHT-VAL
-          RIGHT
-          RESET_FLAG-VAL
-          RESET_FLAG
-))
